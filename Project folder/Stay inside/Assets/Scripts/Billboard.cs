8063486c2fa5aa21e56fba96003de3dba8a4d239
TheMasterLucas <56614267+TheMasterLucas@users.noreply.github.com>
2020-03-18 22:07:56 -0300

    Astethics

---
+++ Project folder/Stay inside/Assets/Scripts/Billboard.cs
@@ -0,0 +1,14 @@
+﻿// Have a nice day ;)
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    public Camera cam;
+
+	void LateUpdate () {
+        var target = cam.transform.position;
+        target.y = transform.position.y;
+        transform.LookAt(target);
+    
+    }
+}
